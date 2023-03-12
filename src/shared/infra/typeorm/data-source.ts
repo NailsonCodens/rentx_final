0f02612df1188f9ad300f82971990701bd87893b
nailson <nailson.ivs@codens.com.br>
2023-03-12 00:06:53 -0300

    update

--- src/shared/infra/typeorm/data-source.ts
+++ src/shared/infra/typeorm/data-source.ts
@@ -14,7 +14,7 @@ import orm from "../../../../orm.json";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.NODE_ENV === "test" ? orm.DB_HOST_MIGRATION : orm.DB_HOST,
-  port: 5432,
+  port: orm.DB_PORT,
   username: orm.DB_USER,
   password: orm.DB_PASSWORD,
   database: process.env.NODE_ENV === "test" ? orm.DB_TEST : orm.DB,
