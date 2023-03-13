2edf35de60e4dff78c329b8bc9d5f1cdad7eeac7
nailson <nailson.ivs@codens.com.br>
2023-03-13 18:57:20 -0300

    add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,7 @@ import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 import "@shared/infra/typeorm/data-source";
 import "@shared/container";
+import cors from "cors";
 
 import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
@@ -20,6 +21,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar/", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars/", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors())
 app.use(routers);
 
 app.use(
