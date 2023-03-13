9775abc05bdf2f31b20904037125ea4719fc3ca1
nailson <nailson.ivs@codens.com.br>
2023-03-13 19:24:51 -0300

    rate limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,9 +10,12 @@ import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
 
 import swaggerFile from "../../../swagger.json";
+import rateLimiter from "./middlewares/rateLimiter";
 import routers from "./routes";
 
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 
@@ -21,7 +24,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar/", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars/", express.static(`${upload.tmpFolder}/cars`));
 
-app.use(cors())
+app.use(cors());
 app.use(routers);
 
 app.use(
